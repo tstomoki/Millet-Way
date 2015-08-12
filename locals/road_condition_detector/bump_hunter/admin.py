e9e716a9ade4ad66fbb8ee06fe023ed9e3ec88db
tstomoki <tstomoki4@gmail.com>
2015-08-12 14:51:53 +0900

    configured display columns on admin

--- locals/road_condition_detector/bump_hunter/admin.py
+++ locals/road_condition_detector/bump_hunter/admin.py
@@ -2,7 +2,12 @@
 from django.contrib import admin
 from bump_hunter.models import LogData, User
 
-admin.site.register(LogData)
-admin.site.register(User)
-
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at',)
+admin.site.register(User, UserAdmin)
+
+class LogDataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lat', 'lon', 'acc_x', 'acc_y', 'acc_z', 'created_at', 'updated_at',)
+admin.site.register(LogData, LogDataAdmin)
