1ce50b36fa81a33e56fd9bab94e3c02002cc1268
tstomoki <tstomoki4@gmail.com>
2015-08-12 14:10:19 +0900

    created initial app files

--- locals/road_condition_detector/road_condition_detector/settings.py
+++ locals/road_condition_detector/road_condition_detector/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'road_condition_detector.bump_hunter',
 )
 
 MIDDLEWARE_CLASSES = (
