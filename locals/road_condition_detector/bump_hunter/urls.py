590cca29f90e4d2101ba62da27e08f255cd70c06
pineplain <hiroya.matsubara@gmail.com>
2015-08-12 18:57:06 +0900

    created bump_sensing view

--- locals/road_condition_detector/bump_hunter/urls.py
+++ locals/road_condition_detector/bump_hunter/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 from bump_hunter import views
 urlpatterns = patterns('',
                        url(r'^bump_map/$', views.bump_map, name='bump_map'),
+                       url(r'^bump_sensing/$', views.bump_sensing, name='bump_sensing'),
                    )
--- locals/road_condition_detector/bump_hunter/views.py
+++ locals/road_condition_detector/bump_hunter/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, render_to_response
 from django import forms
 from django.template import RequestContext
 from django.http import HttpResponse
-## gmapi
-import simplejson as json
 
 def bump_map(request):
     return render_to_response('bump_hunter/bump_map.html',  # 使用するテンプレート
                               context_instance=RequestContext(request))  # その他標準のコンテキスト
+
+def bump_sensing(request):
+    return render_to_response('bump_hunter/bump_sensing.html');
