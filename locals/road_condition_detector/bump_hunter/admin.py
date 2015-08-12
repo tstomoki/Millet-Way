a3f927687895d2bf2644682bd14fc1bfc2f842cc
tstomoki <tstomoki4@gmail.com>
2015-08-12 14:42:11 +0900

    added application models on admin

--- locals/road_condition_detector/bump_hunter/admin.py
+++ locals/road_condition_detector/bump_hunter/admin.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from bump_hunter.models import LogData, User
+
+admin.site.register(LogData)
+admin.site.register(User)
 
 # Register your models here.
