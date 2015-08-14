a35468cc345c7509ecc8a4b6a42b4302900b78c7
pineplain <hiroya.matsubara@gmail.com>
2015-08-14 20:23:41 +0900

    logging settings

--- locals/road_condition_detector/road_condition_detector/settings.py
+++ locals/road_condition_detector/road_condition_detector/settings.py
@@ -127,6 +127,40 @@ STATICFILES_DIRS = (
 )
 
 # Authentification
+
 LOGIN_URL='/bump_hunter/login'
 
 LOGIN_REDIRECT_URL = '/bump_hunter/bump_map'
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(asctime)s [%(levelname)s] %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'bump_hunter':{
+            'handlers': ['console',],
+            'level': 'DEBUG',
+        },
+    },
+}
