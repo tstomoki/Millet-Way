ac3f87dcc1551e9beffd6c9d99b4de587c839580
pineplain <hiroya.matsubara@gmail.com>
2015-08-14 23:34:51 +0900

    bluemix deployment

--- locals/road_condition_detector/run.sh
+++ locals/road_condition_detector/run.sh
@@ -3,6 +3,7 @@ if [ -z "$VCAP_APP_PORT" ];
     then SERVER_PORT=3000;
     else SERVER_PORT="$VCAP_APP_PORT";
 fi
-echo port is $SERVER_PORT
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+echo port is $SERVER_PORT
 python manage.py runserver --noreload 0.0.0.0:$SERVER_PORT
